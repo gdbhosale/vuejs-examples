4d76b108c98ebee7112caa545a6a51f5d1ab4ce9
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-12-13 17:31:19 +0530

    Component Message + Bulma

---
+++ ex9.js
@@ -0,0 +1,36 @@
+Vue.component('message', {
+    props: [
+        'title',
+        'body'
+    ],
+    data() {
+        return {
+            isVisible: true
+        }
+    },
+    template: `
+        <article class="message" >
+            <div class="message-header">
+                {{ title }}
+                <button class="close" @click="hideMessage">x</button>
+            </div>
+            <div class="message-body" v-show="isVisible">
+                {{ body }}
+            </div>
+        </article>
+    `,
+    methods: {
+        hideMessage() {
+            if(this.isVisible) {
+                this.isVisible = false;
+            } else {
+                this.isVisible = true;
+            }
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#root'
+    
+});
